0499e44827466c1bc1250bc5e3877cf40f62085a
Krish Kothari <krishkothari1111@gmail.com>
2026-05-08 17:22:07 +0530

    resolved

--- vite.config.js
+++ vite.config.js
@@ -6,22 +6,22 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api/iss': {
+      '/api/iss-now': {
         target: 'http://api.open-notify.org',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/iss/, ''),
+        rewrite: (path) => path.replace(/^\/api\/iss-now/, '/iss-now'),
         logLevel: 'debug',
       },
-      '/api/news': {
-        target: 'https://newsapi.org/v2',
+      '/api/astros': {
+        target: 'http://api.open-notify.org',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/news/, ''),
+        rewrite: (path) => path.replace(/^\/api\/astros/, '/astros'),
         logLevel: 'debug',
       },
-      '/api/nominatim': {
-        target: 'https://nominatim.openstreetmap.org',
+      '/api/iss-passes': {
+        target: 'http://api.open-notify.org',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/nominatim/, ''),
+        rewrite: (path) => path.replace(/^\/api\/iss-passes/, '/iss-passes'),
         logLevel: 'debug',
       },
     },
